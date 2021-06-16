f4e8c2afaf1808068133617934a7f7a4ee9297a6
Cesar De Greve <cesar.de.greve@student.howest.be>
2021-06-16 15:23:12 +0200

    test2

--- api/game/models/game.js
+++ api/game/models/game.js
@@ -2,7 +2,7 @@
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#lifecycle-hooks)
- * to customize this modell
+ * to customize this modelll
  */
 
 module.exports = {};
